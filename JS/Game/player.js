6746220b85dbbb67e285b8b8528d9a95b670ae83
abstract64 <38624261+abstract64@users.noreply.github.com>
2019-05-30 14:22:37 -0400

    owo

--- JS/Game/player.js
+++ JS/Game/player.js
@@ -1,17 +1,28 @@
+updateList.push({f: () => updatePlayer()});
+resetList.push({f: () => resetPlayer()});
+drawList.push({f: () => drawPlayer, l: 1});
+
 
 const PLAYER_SPEED = 5;
+const PLAYER_RAD = 30;
+
 
 var player1;
 
 function resetPlayer() {
-    player1 = new PlayerClass;
+    player1 = new PlayerClass(200, 200);
 }
 
+function updatePlayer() {
+    player1.update();
+}
 
+function drawPlayer() {
+    player1.draw();
+}
 
 class PlayerClass {
     constructor(x, y) {
-        this.x = x;
         this.y = y;
     }
 
@@ -20,10 +31,21 @@ class PlayerClass {
     }
 
     move() {
-        if (keyboard.key)
+        if (keyboard.KEY_W.held) {
+            this.y -= PLAYER_SPEED;
+        }
+        if (keyboard.KEY_S.held) {
+            this.y += PLAYER_SPEED;
+        }
+        if (keyboard.KEY_A.held) {
+            this.x -= PLAYER_SPEED;
+        }
+        if (keyboard.KEY_D.held) {
+            this.x += PLAYER_SPEED;
+        }
     }
 
     draw() {
-
+        drawCircle(this.x, this.y, PLAYER_RAD, "red");
     }
 }
